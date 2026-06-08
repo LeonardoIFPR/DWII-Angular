7ba265d07c43cfa7c7d8f63a1e33213528246ebb
LeonardoIFPR <20241ctb0100029@estudantes.ifpr.edu.br>
2026-06-08 19:43:18 +0000

    tentar arrumar

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,10 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule} from '@angular/material/card';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [MatListModule, MatCardModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatListModule, MatCardModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
