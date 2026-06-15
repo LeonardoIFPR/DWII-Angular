4526370dbfcdd937b8ffab71500ccbadb5708209
LeonardoIFPR <20241ctb0100029@estudantes.ifpr.edu.br>
2026-06-15 02:55:42 +0000

    melhorias na home do portfolio proximas paginas a melhorar serão contato, projetos e sobre

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -4,16 +4,15 @@ import { MatCardModule} from '@angular/material/card';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenav } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
-
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatListModule, MatCardModule, MatSidenav, MatIconModule, MatMenuModule, MatIconModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule, MatListModule, MatCardModule, MatSidenavModule, MatIconModule, MatMenuModule, MatGridListModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- portfolio-angular/src/app/home/home.ts
+++ portfolio-angular/src/app/home/home.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule],
+  imports: [RouterLink, MatCardModule, MatButtonModule, MatIconModule, MatListModule, MatGridListModule, MatExpansionModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
