e6bc1b005975c4c73b0b0908231d48cfb1c063e9
LeonardoIFPR <20241ctb0100029@estudantes.ifpr.edu.br>
2026-06-15 19:59:48 +0000

    tecnologia com função de buscar por ID pela URL

--- portfolio-angular/api/tecnologias.php
+++ portfolio-angular/api/tecnologias.php
@@ -4,8 +4,22 @@ header("Access-Control-Allow-Origin: *");
 
 require __DIR__ . "/../conexao.php";
 
-$sql = "SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE status = 'ativo' ORDER BY categoria, nome";
-$tecnologias = $pdo->query($sql)->fetchAll();
-
-echo json_encode($tecnologias);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    
+    $stmt = $pdo->prepare("SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE id = ? AND status = 'ativo'");
+    $stmt->execute([$id]); // essa linha serve para mandar o id pro banco e rodar a consulta
+    $tecnologia = $stmt->fetch(); // essa linha que busca os dados e diz se o projeto existe. se não achar nada, o $tecnologia vira false para entrar no if abaixo
+    
+    if (!$tecnologia) {
+        http_response_code(404);
+        echo json_encode(["erro" => "nao encontrado"]);
+        exit;
+    }
+    
+    echo json_encode($tecnologia);
+} else {
+    $tecnologias = $pdo->query("SELECT id, nome, categoria, descricao, ano_criacao FROM tecnologias WHERE status = 'ativo' ORDER BY categoria, nome")->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($tecnologias);
+}
 ?>
