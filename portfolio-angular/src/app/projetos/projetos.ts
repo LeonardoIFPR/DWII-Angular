bc163df0a478d5076d8f16c407e1bd0731b5d9a7
LeonardoIFPR <20241ctb0100029@estudantes.ifpr.edu.br>
2026-06-22 23:52:34 +0000

    Aula18: Angular consumindo a API (Projetos, Catalogo e botao GitHub)

--- portfolio-angular/src/app/projetos/projetos.ts
+++ portfolio-angular/src/app/projetos/projetos.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+/*
+não sei se ocorreu no seu este, problema mas no meu estava dando algum problema que quando 
+eu clicava no link para projetos ele ia e carregava os projetos no array(confirmei isso com um dbug)
+mas a tela so mostrava a mesnagem Carregando projetos... o unico jeito que achei de arrumar
+foi usando esse ChangeDetectorRef e chamando o metodo detectChanges() que força a tela a
+atualizar e mostrar os projetos carregados não sei se fiz algo errado no meu mas 
+imagino que não pois revisei bastante
+*/
+
+import { Component, inject, OnInit, ChangeDetectorRef } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { Projeto, ProjetoService } from '../projeto.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-projetos',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './projetos.html',
   styleUrl: './projetos.css',
 })
-export class Projetos {}
+export class Projetos implements OnInit {
+  private service = inject(ProjetoService);
+  private cdr = inject(ChangeDetectorRef);
+  projetos: Projeto[] = [];
+  carregando = true;
+  erro = "";
+
+  ngOnInit() {
+    this.service.listar().subscribe({
+      next: (lista) => {this.projetos = lista; this.carregando = false; this.cdr.detectChanges();},
+      
+      error: () => {this.erro = "falha ao carregar projetos"; this.carregando = false; this.cdr.detectChanges();}
+    });
+  }
+}
